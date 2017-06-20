be633373523bdd7df1e477d0978a049f7bd7d48d
Ivan Lazar Miljenovic <Ivan.Miljenovic@gmail.com>
2017-06-20 16:58:52 +1000

    Clean up getting stdout and stderr
    It was going to be too difficult to retrofit the old functions to use
    this new one, so just remove them; they were going to go anyway.

--- src/Streaming/Process.hs
+++ src/Streaming/Process.hs
@@ -11,24 +11,20 @@
    Run system commands in a streaming fashion.
 
  -}
-module Streaming.Process
-  ( StdOutErr
-  , withStreamProcess
-  , withStreamCmd
-  ) where
+module Streaming.Process where
 
 import qualified Data.ByteString                    as B
 import           Data.ByteString.Streaming          (ByteString)
 import qualified Data.ByteString.Streaming          as SB
 import           Data.ByteString.Streaming.Internal (defaultChunkSize)
-import           Streaming                          (Of, Stream, hoist)
+import           Streaming                          (hoist)
 import qualified Streaming.Prelude                  as S
 
 import Control.Concurrent.Async.Lifted (Concurrently(..), async,
                                         waitEitherCancel)
 import Control.Monad.Base              (liftBase)
 import Control.Monad.Catch             (MonadMask, finally, onException, throwM)
-import Control.Monad.IO.Class          (MonadIO)
+import Control.Monad.IO.Class          (MonadIO, liftIO)
 import Control.Monad.Trans.Class       (lift)
 import Control.Monad.Trans.Control     (MonadBaseControl)
 import Data.Streaming.Process
@@ -37,41 +33,6 @@ import System.IO                       (Handle, hClose)
 import System.Process                  (CreateProcess(..), shell)
 
 --------------------------------------------------------------------------------
-
--- | A representation of the concurrent streaming of both @stdout@ and
---   @stderr@.
-type StdOutErr m r = ByteString (ByteString m) r
-
--- | Run a process, feeding the provided stream to it and providing
---   both the @stdout@ and @stderr@ to the callback.
---
---   Ideally, this function would just return the resultant
---   'ByteString'; however, it isn't possible to ensure that the
---   handles are successfully closed upon termination in that case.
-withStreamProcess :: (MonadIO m, MonadBaseControl IO m, MonadMask m)
-                     => CreateProcess -> ByteString m r
-                     -> (StdOutErr m () -> m v) -> m ((r, v), ExitCode)
-withStreamProcess cp inp f =
-  do (inH, outH, errH, sph) <- streamingProcess cp
-     res <- runConcurrently ((,) <$> Concurrently (withIn inH)
-                                 <*> Concurrently (withOut outH errH))
-            `finally` (liftBase (hClose outH >> hClose errH))
-            `onException` terminateStreamingProcess sph
-     ec <- waitForStreamingProcess sph
-     return (res, ec)
-  where
-    -- withIn :: Handle -> m r
-    withIn inH = SB.hPut inH inp `finally` liftBase (hClose inH)
-
-    -- withOut :: Handle -> Handle -> m v
-    withOut outH errH = f (getBothBN defaultChunkSize outH errH)
-
--- | A variant of 'withStreamProcess' that runs the provided command
---   in a shell.
-withStreamCmd :: (MonadIO m, MonadBaseControl IO m, MonadMask m)
-                  => String -> ByteString m r
-                  -> (StdOutErr m () -> m v) -> m ((r, v), ExitCode)
-withStreamCmd = withStreamProcess . shell
 
 --------------------------------------------------------------------------------
 
@@ -84,14 +45,14 @@ withStreamCmd = withStreamProcess . shell
 withProcessHandles :: (MonadBaseControl IO m, MonadIO m, MonadMask m, MonadBaseControl IO n)
                       => ByteString m v -> StreamProcess Handle Handle Handle
                       -> (StdOutErr n () -> m r) -> m r
-withProcessHandles inp StreamProcess{..} f =
+withProcessHandles inp sp@StreamProcess{..} f =
   runConcurrently (flip const <$> Concurrently withIn
                               <*> Concurrently withOutErr)
   `finally` liftIO closeOutErr
   where
     withIn = SB.hPut toStdin inp `finally` liftIO (hClose toStdin)
 
-    withOutErr = f (getBothBN defaultChunkSize fromStdout fromStderr)
+    withOutErr = f (getStreamingOutputsN defaultChunkSize sp)
 
     closeOutErr = hClose fromStdout >> hClose fromStderr
 
@@ -111,6 +72,7 @@ withProcessOutput StreamProcess{fromStdout} f =
 
 --------------------------------------------------------------------------------
 
+-- | Represents the inputs and outputs for a streaming process.
 data StreamProcess stdin stdout stderr = StreamProcess
   { toStdin    :: !stdin
   , fromStdout :: !stdout
@@ -138,32 +100,47 @@ withStreamingProcess cp f = do
     ExitSuccess   -> return r
     ExitFailure _ -> throwM (ProcessExitedUnsuccessfully cp ec)
 
+-- | A variant of 'withStreamingProcess' that runs the provided
+--   command in a shell.
+withStreamingCommand :: (InputSource stdin, OutputSink stdout, OutputSink stderr
+                        , MonadIO m, MonadMask m)
+                        => String
+                        -> (StreamProcess stdin stdout stderr -> m r) -> m r
+withStreamingCommand = withStreamingProcess . shell
+
 terminateStreamingProcess :: (MonadIO m) => StreamingProcessHandle -> m ()
 terminateStreamingProcess = liftIO . terminateProcess . streamingProcessHandleRaw
 
 --------------------------------------------------------------------------------
 
-getBothBN :: (MonadBaseControl IO m) => Int -> Handle -> Handle -> ByteString (ByteString m) ()
-getBothBN n h1 h2 = SB.fromChunks . hoist SB.fromChunks . S.partitionEithers
-                    $ getBothN n h1 h2
+-- | A representation of the concurrent streaming of both @stdout@ and
+--   @stderr@.
+--
+--   Note that if for example you wish to completely discard stderr,
+--   you can do so with @'hoist' 'SB.effects'@ (or just process the
+--   stdout, then run 'SB.effects' at the end to discard the stderr).
+type StdOutErr m r = ByteString (ByteString m) r
 
-getBothN :: (MonadBaseControl IO m) => Int -> Handle -> Handle
-            -> Stream (Of (Either B.ByteString B.ByteString)) m ()
-getBothN n h1 h2 | n > 0 = loopBoth
+-- | Get both stdout and stderr concurrently.
+getStreamingOutputsN :: (MonadBaseControl IO m) => Int -> StreamProcess stdin Handle Handle
+                        -> StdOutErr m ()
+getStreamingOutputsN n _ | n <= 0 = return ()
+getStreamingOutputsN n StreamProcess{fromStdout, fromStderr} =
+  SB.fromChunks . hoist SB.fromChunks . S.partitionEithers $ loopBoth
   where
     -- Will block until /something/ is available; may have length < n
-    get1 = liftBase (B.hGetSome h1 n)
-    get2 = liftBase (B.hGetSome h2 n)
+    getOut = liftBase (B.hGetSome fromStdout n)
+    getErr = liftBase (B.hGetSome fromStderr n)
 
-    loopBoth = do !res <- lift (do getA1 <- async get1
-                                   getA2 <- async get2
-                                   waitEitherCancel getA1 getA2)
+    loopBoth = do !res <- lift (do getOutA <- async getOut
+                                   getErrA <- async getErr
+                                   waitEitherCancel getOutA getErrA)
                   -- As soon as either one
                   -- returns empty, then
                   -- focus on the other.
                   case res of
-                    Left  "" -> loopWith Right get2
-                    Right "" -> loopWith Left  get1
+                    Left  "" -> loopWith Right getErr
+                    Right "" -> loopWith Left  getOut
                     _        -> S.yield res >> loopBoth
 
     loopWith f get = go
@@ -172,5 +149,3 @@ getBothN n h1 h2 | n > 0 = loopBoth
                 if B.null b
                    then return ()
                    else S.yield (f b) >> go
-
-getBothN _ _ _ = return ()
